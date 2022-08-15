4338e9f7f25a232b8cdc35bdf1e8efbce2fc049c
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-08-15 22:35:35 +0100

    init portfolio site.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        "custom-black": "#222831",
+        "custom-gray": "#393E46",
+        "custom-teal": "#00ADB5",
+        "custom-space": "#EEEEEE",
+      }
+    },
+  },
+  plugins: [],
+}
