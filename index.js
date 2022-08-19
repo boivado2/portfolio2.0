aad9fd42a7259b59ab2c33749933f6132816e638
John Ayebamondiafere Godwin <johngodwinaye@gmail.com>
2022-08-19 23:44:52 +0100

    implement mobile navigation bar.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dateElement = document.querySelector('span.date')
-
+const hamburger = document.querySelector('.hamburger')
 
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('nav')
@@ -11,5 +11,9 @@ function getCurrentYear() {
   const date = new Date()
   dateElement.textContent = date.getFullYear()
 }
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle("hamburger_active")
+})
 
 getCurrentYear()
